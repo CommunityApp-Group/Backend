6e734750d80ade22671641cf1d1dc221cc7ec1fe
Dr. Brain <samody2006@yahoo.com>
2022-11-20 01:33:27 +0100

    userResources

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -17,9 +17,9 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->encodedKey,
-            'name' => $this->name,
+            'fullname' => $this->fullname,
             'email' => $this->email,
-            'phone' => $this->phone,
+            'call_up_no' => $this->call_up_no,
             'verified' => is_null($this->email_verified_at) ? 'no' : 'yes',
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'permissions' => $this->getPermissionNames()
