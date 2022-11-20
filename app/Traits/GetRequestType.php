c3b774d39e34856ab36f5a7730c0293396123bdc
Dr. Brain <samody2006@yahoo.com>
2022-11-20 01:12:46 +0100

    Notification

--- app/Traits/GetRequestType.php
+++ app/Traits/GetRequestType.php
@@ -2,39 +2,86 @@
 
 namespace App\Traits;
 
-use App\Http\Resources\Post\PostResource;
-use App\Http\Resources\Post\PostResourceCollection;
-
+use App\Http\Resources\Auction\AuctionResource;
+use App\Http\Resources\Auction\AuctionResourceCollection;
+use App\Http\Resources\Story\StoryResource;
+use App\Http\Resources\Story\StoryResourceCollection;
 
 trait GetRequestType
 {
-
-    public function getSimplePost($post)
+    public function getUserDetail($user)
     {
-        if (request()->has('fullDetails') && request('fullDetails') === 'true') {
-            $post = $post->with('user')->firstOrFail();
-            return new PostResourceCollection($post);
-        }
+        // if(request()->has('fullDetails') && request('fullDetails') === 'true') {
+        //     $retrieved_user = $user->with('company', 'userProfile')->jsonPaginate();
+        //     return UserResourceCollection::collection($retrieved_user);
+        // }
 
-        return new PostResource($post->firstOrFail());
-    }
-    public function getFullPost($post)
-    {
-        if (request()->has('fullDetails') && request('fullDetails') === 'true') {
-            $post = $post->with('user')->paginate(20);
-            return PostResourceCollection::collection($post);
-        }
-        return  PostResource::collection($post->with()->paginate(20));
+        // return UserResource::collection($user->jsonPaginate());
     }
 
-    public function getMyPost($post)
+    public function getSimpleUser($user)
+    {
+        // if(request()->has('fullDetails') && request('fullDetails') === 'true') {
+        //     // return new UserResourceCollection($new_user);
+        // }
+
+        // return new UserResource($user->firstOrFail());
+    }
+
+    public function getSimpleAuction($auction)
     {
         if (request()->has('fullDetails') && request('fullDetails') === 'true') {
-            $post = $post->paginate(20);
-
-            return PostResourceCollection::collection($post);
+            $auction = $auction->with('user', 'verifiedBy')->firstOrFail();
+            return new AuctionResourceCollection($auction);
         }
 
-        return  PostResource::collection($post->with()->paginate(20));
+        return new AuctionResource($auction->firstOrFail());
+    }
+    public function getFullAuction($auction)
+    {
+        if (request()->has('fullDetails') && request('fullDetails') === 'true') {
+            $auction = $auction->with('user', 'verifiedBy')->paginate(20);
+            return AuctionResourceCollection::collection($auction);
+        }
+        return  AuctionResource::collection($auction->with('verifiedBy')->paginate(20));
+    }
+
+    public function getMyAuction($auction)
+    {
+        if (request()->has('fullDetails') && request('fullDetails') === 'true') {
+            $auction = $auction->paginate(20);
+
+            return AuctionResourceCollection::collection($auction);
+        }
+        return  AuctionResource::collection($auction->paginate(20));
+    }
+
+    public function getSimpleStory($story)
+    {
+        if (request()->has('fullDetails') && request('fullDetails') === 'true') {
+            $story = $story->with('user')->firstOrFail();
+            return new StoryResourceCollection($story);
+        }
+
+        return new StoryResource($story->firstOrFail());
+    }
+    public function getFullStory($story)
+    {
+        if (request()->has('fullDetails') && request('fullDetails') === 'true') {
+            $story = $story->with('user')->paginate(20);
+            return StoryResourceCollection::collection($story);
+        }
+        return  StoryResource::collection($story->with()->paginate(20));
+    }
+
+    public function getMystory($story)
+    {
+        if (request()->has('fullDetails') && request('fullDetails') === 'true') {
+            $story = $story->paginate(20);
+
+            return StoryResourceCollection::collection($story);
+        }
+
+        return  StoryResource::collection($story->paginate(20));
     }
 }
